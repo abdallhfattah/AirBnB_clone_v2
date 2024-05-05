cae6394cbe8bd204fc1adbef26477f0c05635c8f
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-05 10:39:33 +0300

    added table name as string to avoid cirrcular depedency

--- models/city.py
+++ models/city.py
@@ -3,11 +3,11 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 
 from models.base_model import Base, BaseModel
-from models.state import State
 
 
 class City(BaseModel,Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128) , nullable=False)
-    state_id = Column(String(60) ,ForeignKey(State.id) ,nullable=False )
+    # adding __tablename__.id as string form to avoid circular dependency
+    state_id = Column(String(60) ,ForeignKey("states.id") ,nullable=False )
