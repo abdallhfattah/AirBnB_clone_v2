30cb971e4130b5b5e3967fca642ea67b9c5b620d
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-05 14:43:59 +0300

    back reference to the places model

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from models.base_model import Base, BaseModel
 
@@ -9,5 +10,7 @@ class City(BaseModel,Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128) , nullable=False)
+
     # adding __tablename__.id as string form to avoid circular dependency
     state_id = Column(String(60) ,ForeignKey("states.id") ,nullable=False )
+    places = relationship("Place", backref= "cities", cascade="delete")
