d83d7e5b7b387b857e67481c960fdefc31cf4368
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-05 12:48:56 +0300

    users Colums added

--- models/user.py
+++ models/user.py
@@ -2,12 +2,13 @@
 """This module defines a class User"""
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
 
 
-class User(BaseModel):
+class User(BaseModel , Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128) , nullable=False)
+    password = Column(String(128) , nullable=False)
+    first_name = Column(String(128) , nullable=False)
+    last_name = Column(String(128) , nullable=False)
