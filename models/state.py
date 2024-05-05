12e917632f7ff055b0eb0913ade96766c03720c9
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-05 10:39:59 +0300

    EOF new line

--- models/state.py
+++ models/state.py
@@ -11,11 +11,19 @@ from models.base_model import Base, BaseModel
 from models.city import City
 
 
-class State(BaseModel ,Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128) , nullable=False)
-    cities = relationship(City , backref="state" , cascade="all, delete")
+class State(BaseModel, Base):
+    """Represents a state for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
