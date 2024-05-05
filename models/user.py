2d0831c2ac6e18f57008a3cb49fa4d5f46095c5d
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-05 15:02:48 +0300

    first , last name could be nullable now

--- models/user.py
+++ models/user.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import Base, BaseModel
 
 
-class User(BaseModel , Base):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128) , nullable=False)
-    password = Column(String(128) , nullable=False)
-    first_name = Column(String(128) , nullable=False)
-    last_name = Column(String(128) , nullable=False)
-    places = relationship("Place" , backref="user" , cascade="delete")
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
